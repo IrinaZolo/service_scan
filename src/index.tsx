eaf2ce80202667890941dc6b61528c6cc144dffe
IrinaZolo <ira.zolotareva.97@gmail.com>
2023-05-12 00:59:01 +0200

    deploy two pages

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <Provider store={store}>
-      <BrowserRouter >
+      <BrowserRouter basename='/service_scan/'>
         <React.StrictMode>
           <App />
         </React.StrictMode>
