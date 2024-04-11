47bec956ebc1a818d7fecd98b503da545c9aeea5
IrinaZolo <ira.zolotareva.97@gmail.com>
2024-04-11 10:13:11 +0200

    add changes in routing

--- src/App.tsx
+++ src/App.tsx
@@ -15,17 +15,17 @@ function App() {
       <Header />
       <main className="box-border mx-auto px-[15px] py-0 flex flex-col max-w-[1440px] relative">
         <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/auth" element={<AuthPage />} />
+          <Route path="/service_scan/" element={<MainPage />} />
+          <Route path="/service_scan/auth" element={<AuthPage />} />
           {isAuth ? (
             <>
-              <Route path="/search" element={<SearchPage />} />
-              <Route path="/results" element={<ResultsPage />} />
+              <Route path="/service_scan/search" element={<SearchPage />} />
+              <Route path="/service_scan/results" element={<ResultsPage />} />
             </>
           ) : (
             <>
               <Route
-                path={"/search"}
+                path={"/service_scan/search"}
                 element={
                   <p style={{ height: "calc(100vh - 230px)" }}>
                     Страница недоступна. Вернитесь на главную страницу.
@@ -33,7 +33,7 @@ function App() {
                 }
               />
               <Route
-                path={"/results"}
+                path={"/service_scan/results"}
                 element={
                   <p style={{ height: "calc(100vh - 230px)" }}>
                     Страница недоступна. Вернитесь на главную страницу.
